4032c376080e000bd378fed32d9e0f7d6678e675
DimaCat <dimiiiitriy@gmail.com>
2023-08-07 01:52:30 +0300

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-      byte bottle2 = 16;
-      byte time2 = 2;
-      int bottle = bottle2 / time2; //почему-то в таком случае принимает только инт
-        int a = bottle * 20; //правильно ли я делаю, что пишу просто значения времени, либо нужно объявить каждое число?
-        int b = bottle*24*60;
-        int c = bottle*24*3*60;
-        int d = bottle*24*60*30;
-System.out.println("За 20 минут машина произвела " + a + " штук бутылок");
-        System.out.println("За сутки машина произвела " + b + " штук бутылок"); //разобрался с пробелами
-        System.out.println("За 3 дня машина произвела " + c + " штук бутылок");
-        System.out.println("За месяц машина произвела " + d + " штук бутылок");
-
+   byte bottleColors = 120;
+   byte whiteBottle = 2;
+   byte brownBottle = 4;
+   int  n = bottleColors / (whiteBottle + brownBottle);
+   int nw = n*whiteBottle;
+   int nb = n*brownBottle;
+   System.out.println("В школе, где " + n + " классов, нужно " + nw +  " банок белой краски и " + nb + " банок коричневой краски");
     }
 }
