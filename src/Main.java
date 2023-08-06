863dd5dafc024423535e5ee7362a465c3065e2a7
DimaCat <dimiiiitriy@gmail.com>
2023-08-07 02:02:31 +0300

    Задание 6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-   byte bottleColors = 120;
-   byte whiteBottle = 2;
-   byte brownBottle = 4;
-   int  n = bottleColors / (whiteBottle + brownBottle);
-   int nw = n*whiteBottle;
-   int nb = n*brownBottle;
-   System.out.println("В школе, где " + n + " классов, нужно " + nw +  " банок белой краски и " + nb + " банок коричневой краски");
+   short banana = 5*80;
+   short milk = 105*2;
+   short iceCream = 2*100;
+   short egs = 4*70;
+   int n = banana + milk + iceCream + egs;
+   float c = n/1000f;
+   System.out.println("Вес завтрака в граммах " + n);
+   System.out.println("Вес завтрака в килограммах " + c);
     }
 }
