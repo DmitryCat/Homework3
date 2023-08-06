a612c57dbf397808975f3cf66bc94a26197fcd0c
DimaCat <dimiiiitriy@gmail.com>
2023-08-07 02:15:24 +0300

    Задание 7

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-   short banana = 5*80;
-   short milk = 105*2;
-   short iceCream = 2*100;
-   short egs = 4*70;
-   int n = banana + milk + iceCream + egs;
-   float c = n/1000f;
-   System.out.println("Вес завтрака в граммах " + n);
-   System.out.println("Вес завтрака в килограммах " + c);
+   short weigh = 7*1000;
+   short lightWeigh = 250;
+   short hardWeigh = 500;
+   int a = weigh/lightWeigh;
+   int b = weigh/hardWeigh;
+   int c = (a+b)/2; //не уверен, что правильно сделал уравнение
+   System.out.println("Дней потребуется при дневной потере 250гр - " + a + ", при дневной потере 500гр - " + b + ", а среднее количество дней равно " + c);
     }
 }
